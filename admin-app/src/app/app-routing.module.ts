77d61f492c5cf17b0c41d40ab9185d3994175ada
DESKTOP-LUHF0QE\hunga <khanh.nguyen@bmbsoft.com.vn>
2021-05-10 17:52:42 +0700

    structure project

--- admin-app/src/app/app-routing.module.ts
+++ admin-app/src/app/app-routing.module.ts
@@ -1,79 +1,75 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { BaseComponent } from './views/layout/base/base.component';
-import { AuthGuard } from './core/guard/auth.guard';
-import { ErrorPageComponent } from './views/pages/error-page/error-page.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { BaseComponent } from "./views/layout/base/base.component";
+import { AuthGuard } from "./core/guard/auth.guard";
+import { ErrorPageComponent } from "./views/pages/error-page/error-page.component";
 
 const routes: Routes = [
-  { path:'auth', loadChildren: () => import('./views/pages/auth/auth.module').then(m => m.AuthModule) },
   {
-    path: '',
+    path: "auth",
+    loadChildren: () =>
+      import("./views/pages/auth/auth.module").then((m) => m.AuthModule),
+  },
+  {
+    path: "2",
+    component: BaseComponent,
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+    import("./views/views.module").then((m) => m.ViewsModule),
+  },
+  {
+    path: "",
     component: BaseComponent,
     canActivate: [AuthGuard],
     children: [
       {
-        path: 'dashboard',
-        loadChildren: () => import('./views/pages/dashboard/dashboard.module').then(m => m.DashboardModule)
+        path: "dashboard",
+        loadChildren: () =>
+          import("./views/pages/dashboard/dashboard.module").then(
+            (m) => m.DashboardModule
+          ),
       },
       {
-        path: 'apps',
-        loadChildren: () => import('./views/pages/apps/apps.module').then(m => m.AppsModule)
+        path: "tables",
+        loadChildren: () =>
+          import("./views/pages/tables/tables.module").then(
+            (m) => m.TablesModule
+          ),
       },
       {
-        path: 'ui-components',
-        loadChildren: () => import('./views/pages/ui-components/ui-components.module').then(m => m.UiComponentsModule)
+        path: "icons",
+        loadChildren: () =>
+          import("./views/pages/icons/icons.module").then((m) => m.IconsModule),
       },
       {
-        path: 'advanced-ui',
-        loadChildren: () => import('./views/pages/advanced-ui/advanced-ui.module').then(m => m.AdvancedUiModule)
+        path: "general",
+        loadChildren: () =>
+          import("./views/pages/general/general.module").then(
+            (m) => m.GeneralModule
+          ),
       },
-      {
-        path: 'form-elements',
-        loadChildren: () => import('./views/pages/form-elements/form-elements.module').then(m => m.FormElementsModule)
-      },
-      {
-        path: 'advanced-form-elements',
-        loadChildren: () => import('./views/pages/advanced-form-elements/advanced-form-elements.module').then(m => m.AdvancedFormElementsModule)
-      },
-      {
-        path: 'charts-graphs',
-        loadChildren: () => import('./views/pages/charts-graphs/charts-graphs.module').then(m => m.ChartsGraphsModule)
-      },
-      {
-        path: 'tables',
-        loadChildren: () => import('./views/pages/tables/tables.module').then(m => m.TablesModule)
-      },
-      {
-        path: 'icons',
-        loadChildren: () => import('./views/pages/icons/icons.module').then(m => m.IconsModule)
-      },
-      {
-        path: 'general',
-        loadChildren: () => import('./views/pages/general/general.module').then(m => m.GeneralModule)
-      },
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, 
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
       // { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
-    ]
-  },
-  { 
-    path: 'error',
-    component: ErrorPageComponent,
-    data: {
-      'type': 404,
-      'title': 'Page Not Found',
-      'desc': 'Oopps!! The page you were looking for doesn\'t exist.'
-    }
+    ],
   },
   {
-    path: 'error/:type',
-    component: ErrorPageComponent
+    path: "error",
+    component: ErrorPageComponent,
+    data: {
+      type: 404,
+      title: "Page Not Found",
+      desc: "Oopps!! The page you were looking for doesn't exist.",
+    },
   },
-  { path: '**', redirectTo: 'error', pathMatch: 'full' }
+  {
+    path: "error/:type",
+    component: ErrorPageComponent,
+  },
+  { path: "**", redirectTo: "error", pathMatch: "full" },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: "top" })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
